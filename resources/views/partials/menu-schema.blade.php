8dd79e0af50524246763ed1006be5f3f90218435
Rizki Herda <rizkiherda@gmail.com>
2026-08-03 20:51:45 +0700

    Escape JSON-LD blocks against script-tag breakout (stored XSS)

--- resources/views/partials/menu-schema.blade.php
+++ resources/views/partials/menu-schema.blade.php
@@ -29,5 +29,5 @@
         'itemListElement' => $products,
     ];
 @endphp
-<script type="application/ld+json">{!! json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}</script>
+<script type="application/ld+json">{!! json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT) !!}</script>
 @endif
